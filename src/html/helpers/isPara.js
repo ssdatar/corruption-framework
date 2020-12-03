ad96c22ce67adbb24454d67df79594caf6ffa07f
Saurabh Datar <sdatar2@stanford.edu>
2020-12-02 22:29:52 -0500

    more stuff

---
+++ src/html/helpers/isPara.js
@@ -0,0 +1,3 @@
+module.exports = function(s) {
+  return s.length > 0;
+}
---
+++ src/js/step4.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-new */
+import AOS from 'aos';
+
+import removeMobileHover from './utils/removeMobileHover';
+import wireSocialButtons from './utils/wireSocialButtons';
+
+
+removeMobileHover();
+
+// Add class to html if JS is loaded
+document.querySelector('html').classList.add('js-is-loaded');
+
+// Wire header social if present
+if (document.querySelectorAll('.g-header__share').length) {
+  wireSocialButtons({
+    facebook: '.g-header__share-button--fb',
+    twitter: '.g-header__share-button--tw',
+    mail: '.g-header__share-button--ma',
+  });
+}
+
+AOS.init();
